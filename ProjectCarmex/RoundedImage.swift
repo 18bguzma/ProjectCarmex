171f0c1147600954900308c91ef5858a6880827c
PSabuStryka <parkershuck1@yahoo.com>
2018-03-26 14:47:43 -0500

    qq

---
+++ ProjectCarmex/RoundedImage.swift
@@ -0,0 +1,81 @@
+//
+//  RoundedImage.swift
+//  ProjectCarmex
+//
+//  Created by HSOIMAC-02 on 3/22/18.
+//  Copyright © 2018 GSSystems. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+@IBDesignable class RoundedImage: UIImageView
+{
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        updateCornerRadius()
+    }
+    
+    @IBInspectable var rounded: Bool = false {
+        didSet {
+            updateCornerRadius()
+        }
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat = 0.1 {
+        didSet {
+            updateCornerRadius()
+        }
+    }
+    
+    func updateCornerRadius() {
+        layer.cornerRadius = rounded ? cornerRadius : 0
+        layer.masksToBounds = rounded ? true : false
+    }
+}
+
+@IBDesignable class RoundedView: UIView
+{
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        updateCornerRadius()
+    }
+    
+    @IBInspectable var rounded: Bool = false {
+        didSet {
+            updateCornerRadius()
+        }
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat = 0.1 {
+        didSet {
+            updateCornerRadius()
+        }
+    }
+    
+    func updateCornerRadius() {
+        layer.cornerRadius = rounded ? cornerRadius : 0
+        layer.masksToBounds = rounded ? true : false
+    }
+}
+
+@IBDesignable class MyButton: UIButton
+{
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        updateCornerRadius()
+    }
+    
+    @IBInspectable var rounded: Bool = false {
+        didSet {
+            updateCornerRadius()
+        }
+    }
+    
+    func updateCornerRadius() {
+        layer.cornerRadius = rounded ? frame.size.height / 2 : 0
+    }
+}
