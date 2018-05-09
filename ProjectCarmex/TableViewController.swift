b8b06c634e43f76a28f659f0383b791bd34ad58a
PSabuStryka <parkershuck1@yahoo.com>
2018-05-09 14:39:52 -0500

    q

---
+++ ProjectCarmex/TableViewController.swift
@@ -0,0 +1,45 @@
+//
+//  TableViewController.swift
+//  ProjectCarmex
+//
+//  Created by HSOIMAC-02 on 5/7/18.
+//  Copyright © 2018 GSSystems. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    private func calculateLabelWidth(label: UILabel) -> CGFloat {
+        let labelSize = label.sizeThatFits(CGSize(width: CGFloat.greatestFiniteMagnitude, height: label.frame.height))
+        return labelSize.width
+    }
+
+    private func calculateMaxLabelWidth(labels: [UILabel]) -> CGFloat {
+        return labels.map(calculateLabelWidth).max()!;
+    }
+    
+    private func updateWidthsForLabels(labels: [UILabel]) {
+        let maxLabelWidth = calculateMaxLabelWidth(labels: labels)
+        for label in labels {
+            let constraint = NSLayoutConstraint(item: label,
+                                                attribute: .width,
+                                                relatedBy: .equal,
+                                                toItem: nil,
+                                                attribute: .notAnAttribute,
+                                                multiplier: 1,
+                                                constant: maxLabelWidth)
+            label.addConstraint(constraint)
+        }
+    }
+}
